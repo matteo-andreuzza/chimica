be314b7f7cc97febdcf5e23418abee9dc97bf870
matteo-andreuzza <76852004+matteo-andreuzza@users.noreply.github.com>
2022-12-20 18:21:09 +0100

    Update newgui.py

--- newgui.py
+++ newgui.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sys
 import PySimpleGUI as sg
+import time
 
 def formulaMin():
     data= pd.read_csv("table.csv")
@@ -8,69 +9,80 @@ def formulaMin():
     print(df)
     a = df.set_index('Symbol').to_dict()['AtomicMass']
     print(a)
-
-
-    # Define the window's contents
     layout = [  [sg.Text("inserisci il numero di elementi presenti nel composto")],     # Part 2 - The Layout
                 [sg.Input()],
                 [sg.Button('Ok')] ]
-    # Create the window
     window = sg.Window('Window Title', layout)      # Part 3 - Window Defintion
-
-    # Display and interact with the Window
     event, values = window.read()                   # Part 4 - Event loop or Window.read call
-
-    # Do something with the information gathered
     print('Hello', values[0], "! Thanks for trying PySimpleGUI")
-
-    # Finish up by removing from the screen
     window.close()    
-
-    numero=int(values[0])
-    print("#### Calcolatore formula minima di un composto chimmico ####")
-    
-    
-    layout = [[sg.Button('Ok')] ]
+    try:
+        numero=int(values[0])
+    except ValueError:
+        layout = [  [sg.Text("Non puoi inserire valori diversi da numeri, premi OK per riavviare l'app")],     # Part 2 - The Layout
+            [sg.Button('Ok')] ]
+        window = sg.Window('Conferma riavvio app', layout)      # Part 3 - Window Defintion
+        event, values = window.read()                   # Part 4 - Event loop or Window.read call
+        window.close()   
+        formulaMin()
+    layout = [[sg.Button('Ok')], ]
     for i in range(numero):
         layout.append([sg.Text("inserisci il simbolo dell'elemento " + str(i))],)
         layout.append([sg.Input()],)
         layout.append([sg.Text("inserisci la percentuale dell'elemento" + str(i))],)
         layout.append([sg.Input()],)
-        
-        # Create the window
     window = sg.Window('Window Title', layout)      # Part 3 - Window Defintion
-
-    # Display and interact with the Window
     event, values = window.read()                   # Part 4 - Event loop or Window.read call
-
-    # Do something with the information gathered
     print('Hello', values[0], "! Thanks for trying PySimpleGUI")
-
-    # Finish up by removing from the screen
     window.close() 
-
-    
-    
     lde = []
-    for i in range(numero):
+    for i in range(numero+(numero-1)):
+        if i % 2 != 0:
+            print("skipped")
+            continue
         print("passaggio " + str(i))
         print("numero"  + str(numero))
-        print(str(numero))
         try:
             qsim1 = float(str(values[i+1]))/a[values[i]]
         except ValueError:
-            print("exept")
-            qsim1 = float(str(values[i+2]))/a[values[i+1]]
+            layout = [  [sg.Text("non puoi inserire valori diversi da numeri, conferma riavvio APP")],     # Part 2 - The Layout
+                [sg.Button('Ok')] ]
+            window = sg.Window('Conferma riavvio app', layout)      # Part 3 - Window Defintion
+            event, values = window.read()                   # Part 4 - Event loop or Window.read call
+            window.close()   
+            formulaMin()
         lde.append(qsim1)
         print(lde)
-
     lde.sort()
     qmin = lde[0]
-    lde.reverse()
     print(lde)
+    layout = []
     for i in range(numero):
         print("[debug] indice for : " + str(i))
         print("[debug] posizione lde : " + str(lde[i]))
         per1 = lde[i]  / qmin
-        print("presenza elemento " + str(i+1) + " : " + str(per1))
-formulaMin()
+        layout.append([sg.Print("presenza elemento " + str(i+1) + " : " + str(per1))],)
+    try: 
+        window = sg.Window('Window Title', layout)      # Part 3 - Window Defintion
+    except AttributeError:
+        pass
+    event, values = window.read()                   # Part 4 - Event loop or Window.read call
+    time.sleep(5)
+    layout = [  [sg.Text("Premi OK per chiudere")],     # Part 2 - The Layout
+                [sg.Button('Ok')] ]
+    window = sg.Window('Conferma chiusura', layout)      # Part 3 - Window Defintion
+    event, values = window.read()                   # Part 4 - Event loop or Window.read call
+    window.close()    
+    window.close()    
+try:
+    formulaMin()
+except TypeError:
+    print("errore, restart")
+    layout = [  [sg.Text("Premi OK per far ripartire l'app")],     # Part 2 - The Layout
+            [sg.Button('Ok')] ]
+    window = sg.Window('Conferma chiusura', layout)      # Part 3 - Window Defintion
+    event, values = window.read()                   # Part 4 - Event loop or Window.read call
+    window.close()   
+    formulaMin()
+
+
